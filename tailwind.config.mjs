0227db3290498cfde15a812ac102855562760e5d
Sara <sape0003@stud.kea.dk>
2025-02-05 22:09:12 +0100

    Overskrifter tilføjet til undersider + andet

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -99,9 +99,9 @@ export default {
       spacing: {
         gap: "25px",
         "mobil-bottom": "85px",
-        "desktop-bottom": "100px",
+        "desktop-bottom": "120px",
         "mobil-top": "85px",
-        "desktop-top": "100px",
+        "desktop-top": "120px",
       },
     },
   },
